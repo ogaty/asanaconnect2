8a9954be63a10ebfea0bb773f1ad22777e07eacf
EC2 Default User <ec2-user@ip-172-31-20-172.ap-northeast-1.compute.internal>
2021-09-28 17:03:30 +0900

    dashboard

--- hook.php
+++ hook.php
@@ -36,3 +36,37 @@ function to_asana($post_ID) {
 
 
 add_action( 'publish_post',  'to_asana');
+
+
+add_action('wp_dashboard_setup', 'asana_dashboard_widgets');
+function asana_dashboard_widgets() {
+	wp_add_dashboard_widget('asana_theme_options_widget', 'Asana My Tasks', 'asana_widgets_callback');
+}
+
+function asana_widgets_callback() {
+	$token = get_option('asana_personal_token');
+	$headers = [
+		'Accept: application/json',
+		'Authorization: Bearer ' . $token,
+	];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+          CURLOPT_URL => 'https://app.asana.com/api/1.0/user_task_lists/1126733236832578/tasks?completed_since=' . date('Y-m-d'),
+          CURLOPT_SSL_VERIFYPEER => true,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_HTTPHEADER => $headers,
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+	$tasks = json_decode($response, true);
+
+	echo '<table>';
+
+	foreach ($tasks['data'] as $task) {
+            echo '<tr>';
+	    echo '<td><a href="https://app.asana.com/0/1126733236832578/'. $task['gid'] .'" target="_blank">' . $task['name'] . '</a></td>';
+	    echo '</tr>';
+	}
+	echo '</table>';
+}
